744f18fe622b61f74fd60eafe5513b4f11c6b95f
Bammez <Kingnuel4@gmail.com>
2024-05-03 15:32:53 +0200

    init

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,16 @@
 use std::collections::HashSet;
-
 pub fn find(sum: u32) -> HashSet<[u32; 3]> {
-    unimplemented!("Given the sum {sum}, return all possible Pythagorean triplets, which produce the said sum, or an empty HashSet if there are no such triplets. Note that you are expected to return triplets in [a, b, c] order, where a < b < c");
+    let mut set = HashSet::new();
+    for a in 1..=(sum - 3) / 3 {
+        for b in a + 1..=a + (sum - a * 3 - 1) / 2 {
+            let c = sum - a - b;
+            if a * a + b * b == c * c {
+                set.insert([a, b, c]);
+            }
+        }
+    }
+    set
 }
+
+
+ 
